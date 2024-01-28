69563b47db5ce9a3a1b80b25939ef377b257519e
furry33 <60417722+FurryForWhat@users.noreply.github.com>
2024-01-28 10:46:42 +0630

    Add files via upload

---
+++ Day6.py
@@ -0,0 +1,4 @@
+for j in range(6):
+    for i in range(11):
+        print('#',end=' ')
+    print('')
---
+++ Day6_1.py
@@ -0,0 +1,6 @@
+for l in range(5):
+    for i in range (4 - l):
+        print('-', end=' ')
+    for z in range(l+1):
+        print('*',end= ' ')
+    print()
